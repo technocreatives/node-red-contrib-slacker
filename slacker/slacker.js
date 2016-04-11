66b5d2c577c111ed58e00d4b08bcbacb27941d39
Oskar Hagberg <oskar.hagberg@gmail.com>
2016-04-12 00:28:07 +0200

    Started on a config node

--- slacker/slacker.js
+++ slacker/slacker.js
@@ -1,11 +1,67 @@
 module.exports = function(RED) {
-    function Slacker(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-        this.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
+  'use strict';
+
+  //var Botkit = require('botkit');
+
+  function SlackBotControllerNode(n) {
+      RED.nodes.createNode(this,n);
+
+      // Configuration options passed by Node Red
+      // TODO: make token 'credentials'
+      this.name = n.name;
+      this.token = n.token;
+
+      var node = this;
+
+
+      // Config node states
+      node.connected = false;
+      this.connecting = false;
+      this.closing = false;
+
+      //this.controller = Botkit.slackbot();
+      //this.bot = this.controller.spawn({
+      //  token: this.token
+      //});
+
+      // node.connecting = true;
+      // node.bot.startRTM(function(err,bot,payload) {
+      //   node.connecting = false;
+      //   if (err) {
+      //     //throw new Error('Could not connect to Slack');
+      //     node.connected = false;
+      //   }
+      //   node.connected = true;
+      // });
+
+      this.on('close', function(done) {
+            this.closing = true;
+            // if (this.connected) {
+            //     this.client.once('close', function() {
+            //         done();
+            //     });
+            //     this.client.end();
+            // } else {
+                done();
+            // }
         });
-    }
-    RED.nodes.registerType("slacker",Slacker);
-}
+  }
+
+  RED.nodes.registerType('slack-bot-controller',SlackBotControllerNode);
+
+  function SlackInNode(n) {
+      RED.nodes.createNode(this,n);
+
+      this.controller = RED.nodes.getNode(n.controller);
+
+      var node = this;
+
+      this.on('input', function(msg) {
+          msg.payload = msg.payload.toLowerCase();
+          node.send(msg);
+      });
+  }
+  RED.nodes.registerType('slack-in',SlackInNode);
+
+  
+};
